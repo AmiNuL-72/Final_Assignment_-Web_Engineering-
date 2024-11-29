2db11ff293c3718e6889ced960771373b89c95d6
Md Aminul Islam Amin <157211474+AmiNuL-72@users.noreply.github.com>
2024-11-30 02:53:27 +0600

    Update sidebar.php

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,29 +1,34 @@
+<div class="sidebar clear">
+	<div class="samesidebar clear">
+		<h2>Categories</h2>
+		<ul>
+			<?php 
+				$query = "select * from tbl_category";
+				$category = $db->select($query);
+				if($category){
+					while($result=$category->fetch_assoc()){
+			?>
+			<li><a href="posts.php?category=<?php echo $result['id'];?>">
+			<?php echo $result['name'];?></a></li>
+			<?php }} else{ ?>
+				<li>No Category Created</li>
+			<?php } ?>
+		</ul>
+	</div>
+
 <div class="samesidebar clear">
-				<h2>Latest articles</h2>
-					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
-					</div>
-					
-					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
-					</div>
-					
-					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
-					</div>
-					
-					<div class="popular clear">
-						<h3><a href="#">Post title will be go here..</a></h3>
-						<a href="#"><img src="images/post1.jpg" alt="post image"/></a>
-						<p>Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.Sidebar text will be go here.</p>	
-					</div>
-	
-			</div>
-			
-		</div>
+	<h2>Latest articles</h2>
+	<?php 
+		$query = "select * from tbl_post limit 4";
+		$post = $db->select($query);
+		if($post){
+			while($result=$post->fetch_assoc()){
+	?>
+	<div class="popular clear">
+		<h3><a href="post.php?id=<?php echo $result['id'];?>"><?php echo $result['title'];?></a></h3>
+		<a href="post.php?id=<?php echo $result['id'];?>"><img src="admin/upload/<?php echo $result['image'];?>" alt="post image"/></a>
+		<?php echo $fm->textshort($result['body'],125);?>	
+	</div>
+	<?php }} else{header("Location:404.php");}?>
+	</div>
+</div>
